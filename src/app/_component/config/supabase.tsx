ff923fef30fde25320ab909149bc270cc3dd27a1
Kohei Matsukawa <k.matsukawa@dual.tokyo>
2023-11-19 11:10:29 +0900

    環境変数の参照を調整

--- src/app/_component/config/supabase.tsx
+++ src/app/_component/config/supabase.tsx
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { getServerSideProps } from './importEnv';
 
 // supabaseの初期化を行う
 export const Supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL?? "test",
-  process.env.NEXT_PUBLIC_SUPABASE_API_KEY?? "test",
+  process.env.NEXT_PUBLIC_SUPABASE_URL??"",
+  process.env.NEXT_PUBLIC_SUPABASE_API_KEY??""
 )
